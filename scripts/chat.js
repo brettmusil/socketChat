f66d213591020dfbcc19a252cd5ca9cbe435ad1f
Brett Musil <brettmusil@gmail.com>
2018-07-17 20:35:51 -0500

    first commit

---
+++ scripts/chat.js
@@ -0,0 +1,79 @@
+
+let usernameInput = document.getElementById('');
+
+let username = '';
+let userID = '';
+let users = '';
+// to implement later
+let typing = false;
+
+const usernameForm = document.getElementById('usernameForm');
+const messageForm = document.getElementById('messageForm');
+const messages = document.getElementById('chatMessages');
+let message = '';
+
+var socket = io.connect('http://localhost:3000');
+// var socket = io.connect('http://14a48ab8.ngrok.io');
+
+function focus() {
+    document.getElementById('usernameInput').focus();
+}
+
+function addMessage(msg) {
+    var newMessage = document.createElement('li');
+    var newMessageText = document.createTextNode(msg);
+    newMessage.appendChild(newMessageText);
+    messages.appendChild(newMessage);
+}
+
+usernameForm.onsubmit = function (e) {
+    e.preventDefault();
+    username = document.getElementById('usernameInput').value;
+    if (username != '') {
+        userID = socket.id;
+        messagePrefix = username + ' : ';
+        socket.emit('join chat', username, userID);
+        addMessage('You have joined the chat.');
+        usernameForm.style.display = 'hidden';
+        messageForm.style.display = 'block';
+        document.getElementById('messageInput').focus();
+    }
+}
+
+messageForm.onsubmit = function (e) {
+    e.preventDefault();
+    messageInput = document.getElementById('messageInput');
+    message = messageInput.value;
+    if (message != '') {
+        socket.emit('user sent message', messagePrefix + message);
+        addMessage(messagePrefix + message);
+        window.scrollTo(0, document.body.scrollHeight);
+        messageInput.value = '';
+    }
+}
+
+socket.on('connect', () => {
+    userID = socket.id;
+    socket.emit('hello?', userID);
+});
+
+socket.on('current users', (onlineUsers) => {
+    users = onlineUsers;
+    if (onlineUsers[0]) {
+        addMessage('Online users :')
+        users.forEach(user => {
+            addMessage('    ' + user);
+        });
+    } else {
+        addMessage('There is nobody else here.');
+    }
+});
+
+socket.on('incoming message', (msg) => {
+    addMessage(msg);
+    window.scrollTo(0, document.body.scrollHeight);
+});
+
+socket.on('disconnect', () => {
+    socket.emit('user left', username);
+});
---
+++ server.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+let staticFiles = __dirname + '/scripts';
+
+let currentUsers = [];
+
+let lastUserToLeave = '';
+
+app.use('/', express.static(staticFiles));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/indexo.html');
+});
+
+function getOnlineUsers() {
+    let onlineUsers = [];
+    for (i = 0; i < currentUsers.length; i++) {
+        onlineUsers.push(currentUsers[i][0]);
+    }
+    return onlineUsers;
+}
+
+io.on('connection', function (socket) {
+    socket.on('hello?', (userID) => {
+        io.to(userID).emit('current users', getOnlineUsers());
+    });
+
+    socket.on('join chat', (username, userID) => {
+        socket.broadcast.emit('incoming message', username + ' has joined the chat.');
+        let userdata = [];
+        userdata.push(username, userID);
+        currentUsers.push(userdata);
+        console.log(currentUsers);
+    });
+
+    socket.on('user sent message', (msg) => {
+        socket.broadcast.emit('incoming message', msg);
+    });
+
+    socket.on('disconnect', function () {
+        if (currentUsers.length == 1) {
+            let lastUser = currentUsers[0][0];
+            currentUsers = [];
+            socket.broadcast.emit('incoming message', lastUser + ' has left.');
+            console.log('There are no current users.')
+        } else {
+            Object.keys(io.sockets.sockets).forEach((id) => {
+                for (i = 0; i < currentUsers.length; i++) {
+                    if (currentUsers[i][1] != id) {
+                        let leavingUser = currentUsers[i][0];
+                        currentUsers.splice(i, 1);
+                        socket.broadcast.emit('incoming message', leavingUser + ' has left.');
+                        lastUserToLeave = leavingUser;
+                    }
+                }
+            });
+            console.log('Currently online :' + currentUsers);
+        }
+    });
+
+    /* private message
+    socket.on('say to someone', (id, msg) => {
+      // send a private message to the socket with the given id
+      socket.to(id).emit('my message', msg);
+    });
+    */
+
+});
+
+server.listen(3000);
